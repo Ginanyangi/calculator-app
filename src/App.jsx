0730c1394c9c945bbb7e9dcd6825febf9d709948
Ginanyangi <georginakabasa2018@gmail.com>
2024-04-09 09:35:49 +0300

    add new signs for multiplication and division

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,13 @@ function App() {
           <input type="button" value="AC" onClick={e => setValue('')}/>
           <input type="button" value="DEL" onClick={e => setValue(value.slice(0, -1))}/>
           <input type="button" value="." onClick={e => setValue(value + e.target.value)}/>
-          <input type="button" value="/" onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value="÷" onClick={e => setValue(value + e.target.value)}/>
         </div>
         <div>
         <input type="button" value="7" onClick={e => setValue(value + e.target.value)}/>
         <input type="button" value="8" onClick={e => setValue(value + e.target.value)}/>
         <input type="button" value="9" onClick={e => setValue(value + e.target.value)}/>
-        <input type="button" value="*" onClick={e => setValue(value + e.target.value)}/>
+        <input type="button" value="×" onClick={e => setValue(value + e.target.value)}/>
         </div>
         <div>
         <input type="button" value="4" onClick={e => setValue(value + e.target.value)}/>
